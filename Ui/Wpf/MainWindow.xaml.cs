644de40872879e4b3f8e45d20a560cd380d8b989
CelsoAlmeida <CelsoAlmeida@SYSTEKNA-DEV>
2022-08-12 23:37:42 -0300

    Adicionar arquivos de projeto.

---
+++ Ui/Wpf/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
